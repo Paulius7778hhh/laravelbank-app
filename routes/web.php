a6e6aa334cc1149c9fc488af00e229341a0f4bc5
Paulius7778hhh <114057615+Paulius7778hhh@users.noreply.github.com>
2023-01-29 23:19:30 +0200

    dublicate

--- routes/web.php
+++ routes/web.php
@@ -39,5 +39,5 @@ Route::prefix('admin/account')->name('account-')->group(function () {
     Route::get('/edit/{account}', [A::class, 'edit'])->name('edit');
     Route::put('/edit/{account}', [A::class, 'update'])->name('update');
     Route::delete('/delete/{account}', [A::class, 'destroy'])->name('delete');
-    Route::get('/create', [create::class, 'show'])->name('menu');
+    Route::get('/create/my/something', [create::class, 'show'])->name('menu');
 });
